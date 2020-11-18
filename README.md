b18b031557744a1b2be506f081ad44c1a9eff49f
丛继晔 <congtheking@outlook.com>
2020-11-18 09:59:39 +0000

    init project. Implement common interface

---
+++ README.md
@@ -0,0 +1,43 @@
+# string utils
+
+实现常用字符串操作函数。例如 `Split`, `Join`, `Trim`, `Count`, `Replace`, `ToLower`, `ToUpper` 等。
+
+![](https://img.shields.io/badge/language-c%2B%2B-blue)
+
+## 如何使用
+
+该项目为 header-only 文件，因此只需在文件中包含头文件即可。
+
+### Makefile 使用
+
+```makefile
+CXXFLAGS += -I/path/to/string-utils.h
+```
+
+### CMake 使用
+
+```cmake
+include_directories(/path/to/string-utils-dir)
+```
+
+### 接口说明
+
+函数原型|说明
+--|--
+void Join(T begin, T end, string &res, const string &connector) | 合并为一个字符串，合并标识符为 connector
+string Join(T begin, T end, const string &connector) | 同上，重载函数
+string Join(T src, const string &connector) | 同上，重载函数
+void Split(const string &src, vector<string> &res, const string &seg) | 按照 seg 切分字符串
+vector<string> Split(const string &src, const string &seg) | 同上，重载函数
+string &ToUpper(string &src) | 将字符串全转为大小字母
+string &ToLower(string &src) | 将字符串全转为小写字母
+string &Capitalize(string &src) | 将字符串首字母转为大写字母
+string Center(string &src, size_t width, char fillchar = ' ') | 字符串居中显示，多余部分使用 fillchar 填充
+void Center(string &src, string &dst, size_t width, char fillchar = ' ') | 同上，重载函数
+size_t Count(const string &src, const string &sub, size_t start, size_t end) | 计算子字符串出现的次数
+size_t Count(const string &src, const string &sub) | 同上，重载函数
+bool StartsWith(const string &src, const string &prefix) | 判断字符串是否是以 prefix 开始
+bool EndsWith(const string &src, const string &suffix) | 判断字符串是否是以 suffix 结尾
+string Repeat(const string &str, unsigned int times) | 复制多次字符串
+string Replace(const string &src, const string &target, const string &replacement) | 替换字符串中的字符
+string &Trim(string &src) | 去除字符串首尾空格
---
+++ include/string-utils.h
@@ -0,0 +1,207 @@
+/**
+ * @file string-utils.h
+ * @brief 字符串工具类，常用字符串操作
+ * @author aydon (congtheking@outlook.com)
+ * @version 1.0
+ * @date 2020-11-18
+ * 
+ * @copyright Copyright (c) 2020 aydon
+ */
+#pragma once
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <cctype>
+
+namespace StringUtils
+{
+    using namespace std;
+
+    template <class T>
+    void Join(T begin, T end, string &res, const string &connector)
+    {
+        if (begin == end)
+        {
+            return;
+        }
+        stringstream ss;
+        ss << *begin++;
+        while (begin != end)
+        {
+            ss << connector << *begin++;
+        }
+        res = ss.str();
+    }
+
+    template <class T>
+    string Join(T begin, T end, const string &connector)
+    {
+        string res;
+        Join(begin, end, res, connector);
+        return res;
+    }
+
+    template <class T>
+    string Join(T src, const string &connector)
+    {
+        string res;
+        Join(src.begin(), src.end(), res, connector);
+        return res;
+    }
+
+    inline void Split(const string &src, vector<string> &res, const string &seg)
+    {
+        res.clear();
+        size_t start = 0, end = 0;
+        string sub;
+        while (start < src.size())
+        {
+            if ((end = src.find_first_of(seg, start)) == string::npos)
+            {
+                sub = src.substr(start);
+                res.push_back(sub);
+                return;
+            }
+
+            sub = src.substr(start, end);
+            start = end + seg.size();
+            res.push_back(sub);
+        }
+    }
+
+    inline vector<string> Split(const string &src, const string &seg)
+    {
+        vector<string> res;
+        Split(src, res, seg);
+        return res;
+    }
+
+    inline string &ToUpper(string &src)
+    {
+        transform(src.begin(), src.end(), src.begin(), ::toupper);
+        return src;
+    }
+
+    inline string &ToLower(string &src)
+    {
+        transform(src.begin(), src.end(), src.begin(), ::tolower);
+        return src;
+    }
+
+    inline string &Capitalize(string &src)
+    {
+        transform(src.begin(), src.begin() + 1, src.begin(), ::toupper);
+        return src;
+    }
+
+    inline string Center(string &src, size_t width, char fillchar = ' ')
+    {
+        stringstream ss;
+        ssize_t len = width - src.size();
+        len = len > 0 ? len : 0;
+
+        size_t suffix = len / 2;
+        size_t prefix = len - suffix + src.size();
+        ss << setfill(fillchar) << setw(prefix) << src << setw(suffix) << "";
+        return ss.str();
+    }
+
+    inline void Center(string &src, string &dst, size_t width, char fillchar = ' ')
+    {
+        dst = Center(src, width, fillchar);
+    }
+
+    inline size_t Count(const string &src, const string &sub, size_t start, size_t end)
+    {
+        end = end > src.size() ? src.size() : end;
+        size_t count = 0;
+
+        while (start != string::npos)
+        {
+            start = src.find(sub, start);
+            if (start >= end)
+            {
+                break;
+            }
+            start += sub.size();
+            count++;
+        }
+        return count;
+    }
+
+    inline size_t Count(const string &src, const string &sub)
+    {
+        return Count(src, sub, 0, src.size());
+    }
+
+    inline bool StartsWith(const string &src, const string &prefix)
+    {
+        if (prefix.size() > src.size())
+        {
+            return false;
+        }
+
+        return 0 == src.compare(0, prefix.size(), prefix);
+    }
+
+    inline bool EndsWith(const string &src, const string &suffix)
+    {
+        if (suffix.size() > src.size())
+        {
+            return false;
+        }
+
+        return 0 == src.compare(src.size() - suffix.size(), suffix.size(), suffix);
+    }
+
+    inline string Repeat(const string &str, unsigned int times)
+    {
+        stringstream ss;
+        for (unsigned int i = 0; i < times; ++i)
+        {
+            ss << str;
+        }
+        return ss.str();
+    }
+
+    inline string Replace(const string &src, const string &target, const string &replacement)
+    {
+        return Join(Split(src, target), replacement);
+    }
+
+    inline string &Trim(string &src)
+    {
+        size_t begin = 0,
+               end = src.size();
+        for (size_t i = 0; i < src.size(); ++i)
+        {
+            if (!isspace(src[i]))
+            {
+                begin = i;
+                break;
+            }
+        }
+
+        for (size_t i = end - 1; i > 0; --i)
+        {
+            if (!isspace(src[i]))
+            {
+                end = i;
+                break;
+            }
+        }
+
+        if (begin >= end)
+        {
+            src = "";
+        }
+        else
+        {
+            src = src.substr(begin, end - begin + 1);
+        }
+
+        return src;
+    }
+} // namespace StringUtils
